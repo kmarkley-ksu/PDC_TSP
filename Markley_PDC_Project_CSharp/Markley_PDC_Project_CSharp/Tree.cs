7e7d4a03c2e0f18d9e3dd84a9cab22ac92d986dc
kmarkley-ksu <57118396+kmarkley-ksu@users.noreply.github.com>
2019-12-04 01:20:46 -0500

    Changing the previous name of the folder.
    Renaming the C# version.

---
+++ Markley_PDC_Project_CSharp/Markley_PDC_Project_CSharp/Tree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markley_PDC_Project_CSharp
+{
+    public class Tree
+    {
+        public Node root;
+
+        public Tree(int[] cities, int amount, int partialCost)
+        {
+            this.root = new Node(cities, amount, partialCost);
+        }
+    }
+}
